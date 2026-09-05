e2aafbad6d6bcdfdc9e259c7ac1cc4b9c02e191a
Carlos Rey Romero <70975390+carero2@users.noreply.github.com>
2026-09-05 23:14:58 +0200

    update structure

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastos-v1';
+const CACHE = 'gastos-v2';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png'];
 
 self.addEventListener('install', e => {
